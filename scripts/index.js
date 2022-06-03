34c794a1b33c595489ae97eb98d01e7c1b744600
DenisDemenev <lazer89@list.ru>
2022-06-03 23:35:33 +0500

    первое ревью

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const nameProfile = document.querySelector(".profile__name");
+const titleProfile = document.querySelector(".profile__title");
+const editButton = document.querySelector(".profile__button-edit");
+const popup = document.querySelector(".popup");
+const formInput = popup.querySelector(".popup__form");
+const closeButton = popup.querySelector(".popup__button-close");
+const saveButton = popup.querySelector(".popup__save-button");
+const nameInput = popup.querySelector(".popup__input_type_name");
+const titleInput = popup.querySelector(".popup__input_type_title");
+
+function openPopup() {
+  popup.classList.add("popup__open");
+  nameInput.value = nameProfile.textContent;
+  titleInput.value = titleProfile.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup__open");
+}
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  titleProfile.textContent = titleInput.value;
+  closePopup();
+}
+
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+formInput.addEventListener("submit", formSubmit);
