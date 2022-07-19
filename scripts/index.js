f0e586ccb38495af6e7126f4abcb665e612e4b6c
DenisDemenev <lazer89@list.ru>
2022-07-19 21:43:06 +0500

    Второе ревью 5 спринт

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const nameProfile = document.querySelector(".profile__name");
 const titleProfile = document.querySelector(".profile__title");
 const editButton = document.querySelector(".profile__button-edit");
 const addButton = document.querySelector(".profile__button-add");
-const closeButton = document.querySelectorAll(".popup__button-close");
+const closeButtons = document.querySelectorAll(".popup__button-close");
 
 const popupBigPhoto = document.querySelector(".popup_photo_big");
 const bigPhoto = popupBigPhoto.querySelector(".popup__big-photo");
@@ -23,48 +23,20 @@ const placeNameInput = popupAdd.querySelector(".popup__input_type_place");
 const linkInput = popupAdd.querySelector(".popup__input_type_link");
 
 const photo = document.querySelector("#photoCard").content;
-const photo__card = photo.querySelector(".photo__card");
-const photo__cards = document.querySelector(".photo__cards");
+const photoCard = photo.querySelector(".photo__card");
+const photoCards = document.querySelector(".photo__cards");
 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
-function likeActive(evt) {
+function activeLike(evt) {
   evt.target.classList.toggle("photo__card-like_active");
 }
 
-function cardDelete(el) {
+function deleteCard(el) {
   el.remove();
 }
 
 function openPopup(popupItem) {
   popupItem.classList.add("popup_open");
 }
-
 
 function openPopupProfile() {
   nameInput.value = nameProfile.textContent;
@@ -73,8 +45,7 @@ function openPopupProfile() {
 }
 
 function openPopupAdd() {
-  placeNameInput.value = "";
-  linkInput.value = "";
+  formInputAdd.reset();
   openPopup(popupAdd);
 }
 
@@ -82,33 +53,33 @@ function closePopup(popupItem) {
   popupItem.classList.remove("popup_open");
 }
 
-function formSubmitProfile(evt) {
+function submitFormProfile(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   titleProfile.textContent = titleInput.value;
   closePopup(popupProfile);
 }
 
-function formSubmitAdd(evt) {
+function submitFormAdd(evt) {
   evt.preventDefault();
-  let newInfo = {
+  const newInfo = {
     name: placeNameInput.value,
     link: linkInput.value,
   };
   const newCard = createCard(newInfo);
-  photo__cards.prepend(newCard);
+  photoCards.prepend(newCard);
   closePopup(popupAdd);
 }
 
 function createCard(el) {
-  const card = photo__card.cloneNode(true);
+  const card = photoCard.cloneNode(true);
   const cardPhoto = card.querySelector(".photo__card-image");
   card.querySelector(".photo__card-title").textContent = el.name;
   cardPhoto.src = el.link;
   cardPhoto.alt = el.name;
-  card.querySelector(".photo__card-like").addEventListener("click", likeActive);
+  card.querySelector(".photo__card-like").addEventListener("click", activeLike);
   card.querySelector(".photo__card-remove").addEventListener("click", () => {
-    cardDelete(card);
+    deleteCard(card);
   });
   cardPhoto.addEventListener("click", function () {
     bigPhoto.src = el.link;
@@ -121,14 +92,14 @@ function createCard(el) {
 
 initialCards.forEach(function (el) {
   const placeEl = createCard(el);
-  photo__cards.append(placeEl);
+  photoCards.append(placeEl);
 });
 
 editButton.addEventListener("click", openPopupProfile);
 addButton.addEventListener("click", openPopupAdd);
-formInputProfile.addEventListener("submit", formSubmitProfile);
-formInputAdd.addEventListener("submit", formSubmitAdd);
-closeButton.forEach((btn) => {
+formInputProfile.addEventListener("submit", submitFormProfile);
+formInputAdd.addEventListener("submit", submitFormAdd);
+closeButtons.forEach((btn) => {
   const popup = btn.closest(".popup");
   btn.addEventListener("click", () => closePopup(popup));
 });
